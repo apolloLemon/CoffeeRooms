e7eb5938f48ae77be95cf2e601b14380e1e29572
Matthew Coyle <matthew@coyle.fr>
2021-03-03 13:17:44 +0100

    fix

--- client.java
+++ client.java
@@ -3,38 +3,75 @@ chaîne de caractères c (terminée par un newline) à la machine s sur le port 
 longueur de c sous la forme d’un entier */
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class client {
+	static class Sender extends Thread {
+		Scanner scn = new Scanner(System.in);
+		PrintWriter send;
+		Sender(PrintWriter send){
+			this.send = send;
+		}
+
+		public void run(){
+			while(true){
+				String msg = scn.nextLine();
+				try {
+					send.println(msg);
+				} catch (Exception e){
+					System.out.println("Sender PB");
+				}
+			}
+		}
+	}
+	static class Reciever extends Thread {
+		BufferedReader recieve;
+		Reciever(BufferedReader recieve){
+			this.recieve=recieve;
+		}
+
+		public void run(){
+			while(true){
+				try {
+					String msg = recieve.readLine();
+					System.out.println(msg);
+				} catch (IOException e){
+					System.out.println("Reciever PB");
+				}
+			}
+		}
+	}
+
+
 	public static void main(String[] argu) {
 	
 		Socket so;
-		DataInputStream entree;
-		PrintWriter sortie;
+		Reciever entree;
+		Sender sortie;
 		String s; // le serveur
 		int p; // le port de connexion
-		String ch; // la chaîne envoyée
 		int l; // et sa longueur reçue
 		
-		if (argu.length == 3) { // on récupère les paramètres
+		if (argu.length == 2) { // on récupère les paramètres
 			s=argu[0];
 			p=Integer.parseInt(argu[1]);
-			ch=argu[2];
 			
 			try{// on connecte un socket
 				so = new Socket(s, p);
-				sortie = new PrintWriter(so.getOutputStream(), true);
-				entree = new DataInputStream(so.getInputStream());
-				sortie.println(ch); // on écrit la chaîne et le newline dans le canal de sortie
-				l = entree.readInt(); // on lit l’entier qui arrive
-				System.out.println("D’après le serveur la longueur de "+ch+" est "+l);
-				so.close(); // on ferme la connexion
+				sortie = new Sender		(new PrintWriter(so.getOutputStream(), true));
+				entree = new Reciever	(new BufferedReader(new InputStreamReader(so.getInputStream())));
+				
+				sortie.start();
+				entree.start();
+
+
 			} catch(UnknownHostException e) { 
 				System.out.println(e);
 			} catch (IOException e) {
 				System.out.println("Aucun serveur n’est rattaché au port ");
 			}
 		} else {
-			System.out.println("syntaxe d’appel java cliTexte serveur port chaine_de_caractères\n");
+			System.out.println("syntaxe d’appel java client serveur port\n");
 		} 
 	} 
 }
--- server.java
+++ server.java
@@ -2,36 +2,87 @@
 de la chaîne de caractères que lui envoie un client. La chaîne envoyée se termine par un newline.*/
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class server {
+	static Vector<ClientHandler> clients = new Vector<>();
+	static int counter=0;
+	
+	static class ClientHandler extends Thread {
+		int num;
+		Socket s;
+		BufferedReader entree;
+		DataOutputStream sortie;
+
+		ClientHandler(int n,Socket s, BufferedReader in, DataOutputStream out){
+			this.num=n;
+			this.s=s;
+			this.entree=in;
+			this.sortie=out;
+		}
+
+		public void run() {
+			while(!s.isClosed())
+				try {
+
+					String ch; // la chaîne reçue
+					ch = entree.readLine(); // on lit ce qui arrive
+					System.out.println(ch);
+					for(ClientHandler oc : server.clients){
+						if(oc.isAlive() && oc != this){
+							System.out.println("sending "+ch);
+							oc.sortie.writeUTF("client "+num+" :"+ch+"\n");
+						}
+					}
+					
+				} catch (IOException e) {
+					System.out.println("problème\n"+e);
+				}
+
+		}
+
+	}
+
 	
 	public static void main(String[] argu) {
 		int p; // le port d’écoute
 		ServerSocket ecoute;
 		Socket so;
-		BufferedReader entree;
-		DataOutputStream sortie;
-		String ch; // la chaîne reçue
+
+
+
+//		BufferedReader entree;
+//		DataOutputStream sortie;
 	
 		if (argu.length == 1) {
+		
+			p=Integer.parseInt(argu[0]); // on récupère le port
 			try {
-				p=Integer.parseInt(argu[0]); // on récupère le port
 				ecoute = new ServerSocket(p); // on crée le serveur
 				System.out.println("serveur mis en place ");
 				
 				while (true) {// le serveur va attendre qu’une connexion arrive
+					System.out.println("waiting for new Client ");
 					so = ecoute.accept();
-					entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
-					sortie = new DataOutputStream (so.getOutputStream());
-					ch = entree.readLine(); // on lit ce qui arrive
-					System.out.println("on a reçu : |"+ch+"|");
-					sortie.writeInt(ch.length()); // on renvoie la longueur de la chaîne
-					so.close();
-					System.out.println("on a envoyé : "+ch.length()+" et on a fermé la connexion");
+					System.out.println("new Client ");	
+					ClientHandler client = new ClientHandler(counter++,so,
+						new BufferedReader(new InputStreamReader(so.getInputStream())),
+						new DataOutputStream (so.getOutputStream()));
+
+					clients.add(client);
+					client.start();
+					client.sortie.writeUTF("connected\n");
+
+					//remove dead threads
+					for(ClientHandler c: clients)
+						if(!c.isAlive())
+							clients.remove(c);
+
 				}
-			} catch (IOException e) {
-				System.out.println("problème\n"+e); 
+			} catch (Exception e){
+				System.out.println("something went wrong");
 			}
+		
 		} else { System.out.println("syntaxe d’appel java servTexte port\n"); } 
 	} 
 }
